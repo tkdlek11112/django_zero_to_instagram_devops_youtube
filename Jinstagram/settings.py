0f26d0fcbd0044874546719ad66ff808a5efe283
mychew__ <tkdlek11112@gmail.com>
2023-03-19 21:35:13 +0900

    :art: DB설정 변경

--- Jinstagram/settings.py
+++ Jinstagram/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'Jinstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '43.201.110.107',
+        'NAME': 'youtube_devops',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
